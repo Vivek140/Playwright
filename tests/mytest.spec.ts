7c8af60596a40284c1d51872a4a6a5b28421cf8e
Vivek140 <vivekgupta140597@gmail.com>
2026-06-26 14:03:36 +0530

    fixed

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -3,4 +3,4 @@ import {test, expect} from "@playwright/test";
 test("verify page title", async({page}) => {
     await page.goto("https://www.flipkart.com/");
     await expect(page).toHaveTitle('Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!');
-})
+}) 
